ca46f6ad3e34f58859c454e937eefd3bcb06d21c
Rhapsody0128 <rhapsody0128@gmail.com>
2020-06-01 09:36:35 +0800

    蕃茄鐘

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link>|
+      <router-link to="/list">清單</router-link>|
+      <router-link to="/settings">設定</router-link>
+    </div>
+    <keep-alive>
+      <router-view />
+    </keep-alive>
+  </div>
+</template>
+
+<style>
+* {
+  text-align: center;
+  font-size: 30px;
+}
+</style>
---
+++ src/views/Settings.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="setting">
+    <b-table :items='items' :fields="fields">
+      <template v-slot:cell(preview)="data">
+        <audio controls :src="'./alarms/'+ data.item.file"></audio>
+      </template>
+      <template v-slot:cell(select)="data">
+        <font-awesome-icon v-if="data.item.file == alarm" :icon="['fas','check']"></font-awesome-icon>
+      </template>
+    </b-table>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      items: [
+        {
+          name: '鬧鈴',
+          file: 'alarm1.mp3'
+        },
+        {
+          name: '阿罵',
+          file: 'ama.mp3'
+        },
+        {
+          name: '感覺',
+          file: 'nofeel.mp3'
+        }
+      ],
+      fields: [
+        {
+          key: 'name',
+          label: '名稱'
+        },
+        {
+          key: 'preview',
+          label: '預覽'
+        },
+        {
+          key: 'select',
+          label: '選擇'
+        }
+      ]
+    }
+  },
+  computed: {
+    alarm () {
+      return this.$store.getters.alarm
+    },
+    methods: {
+      selectAlarm (item) {
+        this.$store.commit('selectAlarm', item.file)
+      }
+
+    }
+  }
+}
+</script>
